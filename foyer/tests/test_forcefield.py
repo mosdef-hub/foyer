15f917da9d49040c21dacd55859788b374319aca
matt.thompson@vanderbilt.edu <matt.thompson@vanderbilt.edu>
2017-06-09 09:22:45 -0500

    Update tests

--- foyer/tests/test_forcefield.py
+++ foyer/tests/test_forcefield.py
@@ -80,7 +80,7 @@ def test_preserve_resname():
 
 def test_from_mbuild_customtype():
     mol2 = mb.load(get_fn('ethane_customtype.pdb'))
-    customtype_ff = Forcefield(forcefield_files = get_fn('validate_customtypes.xml'))
+    customtype_ff = Forcefield(forcefield_files=get_fn('validate_customtypes.xml'))
     ethane = customtype_ff.apply(mol2)
 
     assert sum((1 for at in ethane.atoms if at.type == 'C3')) == 2
@@ -93,8 +93,9 @@ def test_from_mbuild_customtype():
     assert all(x.type for x in ethane.dihedrals)
 
 def test_improper_dihedral():
-    mol2 = pmd.load_file(get_fn('benzene.mol2'), structure=True)
-    ff_improper = Forcefield(forcefield_files = get_fn('improper_dihedral.xml'))
-    benzene = ff_improper.apply(mol2) 
-    assert len(benzene.angles) == 6
-    assert len(benzene.dihedrals) == 6
+    untyped_benzene = pmd.load_file(get_fn('benzene.mol2'), structure=True)
+    ff_improper = Forcefield(forcefield_files=get_fn('improper_dihedral.xml'))
+    benzene = ff_improper.apply(untyped_benzene)
+    assert len(benzene.dihedrals) == 18
+    assert len([dih for dih in benzene.dihedrals if dih.improper]) == 6
+    assert len([dih for dih in benzene.dihedrals if not dih.improper]) == 12
