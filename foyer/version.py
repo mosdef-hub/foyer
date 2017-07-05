0f339452dc1f031158130b340107fcc96c11635d
Ray Matsumoto <raymatsumoto@ram.local>
2017-07-05 09:30:34 -0500

    Changed Class for atomtypes 151, 152, 153 on 'oplsaa.xml'

---
+++ foyer/version.py
@@ -0,0 +1,7 @@
+
+# This file is generated in setup.py at build time.
+version = '0.3.1'
+short_version = '0.3.1'
+full_version = '0.3.1'
+git_revision = '1271e10973d4741b3b9a7a0ec3f4c2b3d9ae0052'
+release = True
