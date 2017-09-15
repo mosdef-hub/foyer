beeae73ae12ad894427b9889ff92977f2aee2597
ftiet <ftiet21@gmail.com>
2017-09-15 15:53:01 -0500

    Clean up

--- foyer/version.py
+++
@@ -1,7 +0,0 @@
-
-# This file is generated in setup.py at build time.
-version = '0.3.0'
-short_version = '0.3.0'
-full_version = '0.3.0+2fb3bce5d7eed5f01a1671447d10847a350862f4'
-git_revision = '2fb3bce5d7eed5f01a1671447d10847a350862f4'
-release = False
