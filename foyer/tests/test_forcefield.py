19f12cdf31c374a7e7357982bf47d475908db6a6
Andrew Summers <andrew.z.summers@gmail.com>
2018-05-17 09:55:28 -0500

    Updated unit test for precedence adherance

--- foyer/tests/test_forcefield.py
+++ foyer/tests/test_forcefield.py
@@ -146,16 +146,29 @@ def test_independent_residues_atoms():
     assert _check_independent_residues(topo)
 
 def test_topology_precedence():
-    """Test to see if topology precedence is properly adhered to."""
-    butane = Alkane(4)
-    ff = Forcefield(forcefield_files=get_fn('butane-precedence.xml'))
-    typed_butane = ff.apply(butane)
+    """Test to see if topology precedence is properly adhered to.
 
-    assert len([bond for bond in typed_butane.bonds
+    This test uses a force field file where bond, angle, and dihedral
+    parameters are present with different counts of `type` definitions.
+    It checks that:
+        1. The parameters with the higher number of `type` definitions
+           are assigned (because they are given the highest precedence)
+        2. That if multiple definitions exist with the same number of
+           `type` definitions, that the convention from OpenMM is followed
+           whereby the definitions that occurs earliest in the XML is
+           assigned.
+    """
+    ethane = mb.load(get_fn('ethane.mol2'))
+    ff = Forcefield(forcefield_files=get_fn('ethane-topo-precedence.xml'))
+    typed_ethane = ff.apply(ethane)
+
+    assert len([bond for bond in typed_ethane.bonds
                 if round(bond.type.req, 2) == 1.15]) == 6
-    assert len([angle for angle in typed_butane.angles
-                if round(angle.type.theteq, 3) == 143.239]) == 6
-    assert len([angle for angle in typed_butane.angles
-                if round(angle.type.theteq, 3) == 114.592]) == 4
-    assert len([rb for rb in typed_butane.rb_torsions
-                if round(rb.type.c0, 3) == 0.239]) == 12
+    assert len([bond for bond in typed_ethane.bonds
+                if round(bond.type.req, 2) == 1.6]) == 1
+    assert len([angle for angle in typed_ethane.angles
+                if round(angle.type.theteq, 3) == 120.321]) == 6
+    assert len([angle for angle in typed_ethane.angles
+                if round(angle.type.theteq, 3) == 97.403]) == 6
+    assert len([rb for rb in typed_ethane.rb_torsions
+                if round(rb.type.c0, 3) == 0.287]) == 9
