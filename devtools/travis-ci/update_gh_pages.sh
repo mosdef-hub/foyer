3f79ce3a3a7c77b6f44148e9b9bdb308911339fb
Andrew Z. Summers <andrew.z.summers@gmail.com>
2018-08-09 15:07:26 -0500

    Testing deployment

--- devtools/travis-ci/update_gh_pages.sh
+++ devtools/travis-ci/update_gh_pages.sh
@@ -67,12 +67,12 @@ git commit --allow-empty -m "Deploy to GitHub Pages: ${SHA}"
 ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
 ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 
-ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
-ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
+#ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
+#ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
 echo $ENCRYPTION_LABEL
 echo $ENCRYPTED_IV_VAR
-echo $ENCRYPTED_IV
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in $DIR/deploy_key.enc -out deploy_key -d
+echo $DIR
+openssl aes-256-cbc -K $ENCRYPTED_KEY_VAR -iv $ENCRYPTED_IV_VAR -in $DIR/deploy_key.enc -out deploy_key -d
 chmod 600 deploy_key
 eval `ssh-agent -s`
 ssh-add deploy_key
