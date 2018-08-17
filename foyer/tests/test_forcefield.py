e3c3a74d55fffe94462ba4227887fc8f3da195ab
Andrew Z. Summers <andrew.z.summers@gmail.com>
2018-08-16 23:02:01 -0500

    Multiple DOIs per atomtype (#168)
    * Added support for listing multiple DOIs per atomtype
    
    * Added test for multiple DOI-per-atomtype
    
    * Add *.bib to manifest file
    
    * More readable names when unpacking atomtype references dict
    
    * Fix indenting in test for multiple atom type references
    
    * Simplify difflib usage
    
    * Slight syntax change when logging dois

--- foyer/tests/test_forcefield.py
+++ foyer/tests/test_forcefield.py
@@ -1,3 +1,4 @@
+import difflib
 import glob
 import os
 from pkg_resources import resource_filename
@@ -73,6 +74,18 @@ def test_write_refs():
     oplsaa = Forcefield(name='oplsaa')
     ethane = oplsaa.apply(mol2, references_file='ethane.bib')
     assert os.path.isfile('ethane.bib')
+
+def test_write_refs_multiple():
+    mol2 = mb.load(get_fn('ethane.mol2'))
+    oplsaa = Forcefield(forcefield_files=get_fn('refs-multi.xml'))
+    ethane = oplsaa.apply(mol2, references_file='ethane-multi.bib')
+    assert os.path.isfile('ethane-multi.bib')
+    with open(get_fn('ethane-multi.bib')) as file1:
+        with open('ethane-multi.bib') as file2:
+            diff = list(difflib.unified_diff(file1.readlines(),
+                                             file2.readlines(),
+                                             n=0))
+    assert not diff
 
 def test_preserve_resname():
     untyped_ethane = pmd.load_file(get_fn('ethane.mol2'), structure=True)
