97ccb3b6fa6e5aabe4c3cccbd06d4ded401a8d3f
Alex Yang <ahy3nz@virginia.edu>
2019-03-29 17:05:44 -0500

    *Generate Topology.Potential objects from XML into the foyer.Forcefield object *Parametrize Topology.Connection objects based on foyer.Forcefield.atomtypes/bondtypes

--- foyer/forcefield.py
+++ foyer/forcefield.py
@@ -19,13 +19,14 @@ import parmed as pmd
 import simtk.openmm.app.element as elem
 import foyer.element as custom_elem
 import topology 
-import simtk.unit as u
-from simtk import openmm as mm
-from simtk.openmm import app
-from simtk.openmm.app.forcefield import (NoCutoff, CutoffNonPeriodic, HBonds,
-                                         AllBonds, HAngles, NonbondedGenerator,
-                                         _convertParameterToNumber)
-
+import unyt as u
+#import simtk.unit as u
+#from simtk import openmm as mm
+#from simtk.openmm import app
+#from simtk.openmm.app.forcefield import (NoCutoff, CutoffNonPeriodic, HBonds,
+#                                         AllBonds, HAngles, NonbondedGenerator,
+#                                         _convertParameterToNumber)
+#
 from foyer.atomtyper import find_atomtypes
 from foyer.exceptions import FoyerError
 from foyer import smarts
@@ -238,6 +239,10 @@ class Forcefield(object):
     name : str, optional, default=None
         Name of a forcefield to load that is packaged within foyer.
 
+    atomtypes: list of Topology.AtomType
+    bondtypes : list of Topology.BondType
+    angletypes : list of Topology.AngleType
+
 
     """
     def __init__(self, forcefield_files=None, name=None, validation=True, debug=False):
@@ -247,6 +252,9 @@ class Forcefield(object):
         self.atomTypeRefs = dict()
         self._included_forcefields = dict()
         self.non_element_types = dict()
+        self.atomtypes = list()
+        self.bondtypes = list()
+        self.angletypes = list()
 
         all_files_to_load = []
         if forcefield_files is not None:
@@ -268,10 +276,100 @@ class Forcefield(object):
         if validation:
             for ff_file_name in preprocessed_files:
                 Validator(ff_file_name, debug)
-
+        for xml in preprocessed_files:
+            self._generate_potential_terms_from_xml(xml)
         #super(Forcefield, self).__init__(*preprocessed_files)
         self.parser = smarts.SMARTS(self.non_element_types)
         #self._SystemData = self._SystemData()
+
+    def _generate_potential_terms_from_xml(self, xml):
+        """ From a given xml file, create the topology.Potential objects """
+        tree = ET.parse(xml)
+        root = tree.getroot()
+        self.atomtypes.extend(self._generate_atomtypes(root))
+        self.bondtypes.extend(self._generate_bondtypes(root))
+        self.angletypes.extend(self._generate_angletypes(root))
+
+    def _generate_atomtypes(self, root):
+        """ From an ET, generate topology.AtomType terms"""
+        all_atypes =[]
+        for atype_ele in root.findall('AtomType'):
+            expression = atype_ele.get('expression', default=None)
+            ivars = atype_ele.get('independent_variables', default=None)
+            mass = atype_ele.get('mass', default="0")
+            charge = atype_ele.get('charge', default=None)
+            name = atype_ele.get('name', default=None)
+            parameters = {key:_parse_unyt(val) for key,val in atype_ele.items() 
+                          if (key != 'expression' and 
+                              key != 'independent_variables' and
+                              key != 'mass' and
+                              key != 'charge' and
+                              key != 'name') }
+            new_atype = topology.AtomType(name=name,
+                                        expression=expression, 
+                                        parameters=parameters,
+                                        independent_variables=ivars
+                                        mass=float(mass) * u.amu,
+                                        charge=charge)
+            all_atypes.append(new_atype)
+        return all_atypes
+
+
+    def _generate_bondtypes(self, root):
+        """ From an ET, generate topology.BondType terms"""
+        all_btypes =[]
+        for btype_ele in root.findall('BondType'):
+            expression = btype_ele.get('expression', default=None)
+            ivars = btype_ele.get('independent_variables', default=None)
+            parameters = {key:_parse_unyt(val) for key,val in btype_ele.items() 
+                          if (key != 'expression' and 
+                              key != 'independent_variables')}
+            if len(btype_ele.getchildren()) > 0:
+                for subele in btype_ele.getchildren():
+                    parameters.update({key:_parse_unyt(val) 
+                                        for key,val in subele.items() 
+                                       if (key !='expression' and 
+                                           key !='independent_variables')
+                                      })
+                    expression = subele.get('expression', default=expression)
+                    ivars = subele.get('independent_variables', default=ivars)
+                    new_btype = topology.BondType(expression=expression, 
+                                            parameters=copy.copy(parameters), 
+                                            independent_variables=ivars)
+                    all_btypes.append(new_btype)
+            else:
+                new_btype = topology.BondType(expression=expression, parameters=parameters,
+                                        independent_variables=ivars)
+                all_btypes.append(new_btype)
+        return all_btypes
+
+    def _generate_angletypes(self, root):
+        """ From an ET, generate topology.AngleType terms"""
+        all_angtypes =[]
+        for angtype_ele in root.findall('AngleType'):
+            expression = angtype_ele.get('expression', default=None)
+            ivars = angtype_ele.get('independent_variables', default=None)
+            parameters = {key:_parse_unyt(val) for key,val in angtype_ele.items() 
+                          if (key != 'expression' and 
+                              key != 'independent_variables')}
+            if len(angtype_ele.getchildren()) > 0:
+                for subele in angtype_ele.getchildren():
+                    parameters.update({key:_parse_unyt(val) 
+                                        for key,val in subele.items() 
+                                       if (key !='expression' and 
+                                           key !='independent_variables')
+                                      })
+                    expression = subele.get('expression', default=expression)
+                    ivars = subele.get('independent_variables', default=ivars)
+                    new_angtype = topology.AngleType(expression=expression, 
+                                            parameters=copy.copy(parameters), 
+                                            independent_variables=ivars)
+                    all_angtypes.append(new_angtype)
+            else:
+                new_angtype = topology.AngleType(expression=expression, parameters=parameters,
+                                        independent_variables=ivars)
+                all_angtypes.append(new_angtype)
+        return all_angtypes
 
     @property
     def included_forcefields(self):
@@ -341,7 +439,7 @@ class Forcefield(object):
             dois = set(doi.strip() for doi in parameters['doi'].split(','))
             self.atomTypeRefs[name] = dois
 
-    def apply(self, topology, references_file=None, use_residue_map=True,
+    def apply(self, top, references_file=None, use_residue_map=True,
               assert_bond_params=True, assert_angle_params=True,
               assert_dihedral_params=True, assert_improper_params=False,
               *args, **kwargs):
@@ -349,7 +447,7 @@ class Forcefield(object):
 
         Parameters
         ----------
-        topology : openmm.app.Topology or parmed.Structure or mbuild.Compound
+        top : topoology.Topology or mbuild.Compound
             Molecular structure to apply the force field to
         references_file : str, optional, default=None
             Name of file where force field references will be written (in Bibtex
@@ -380,85 +478,132 @@ class Forcefield(object):
             raise FoyerError('Attempting to atom-type using a force field '
                     'with no atom type defitions.')
 
-        if not (isinstance(topology, top.Topology) and 
-                isinstance(topology, mb.Compound)):
+        if not (isinstance(top, topology.Topology) and 
+                isinstance(top, mb.Compound)):
             from topology.external.convert_mbuild import from_mbuild
-            topology = from_mbuild(topology)
+            top = from_mbuild(top)
             #residues = kwargs.get('residues')
             #topology, positions = generate_topology(topology,
             #        self.non_element_types, residues=residues)
         #else:
             #positions = np.empty(shape=(topology.getNumAtoms(), 3))
             #positions[:] = np.nan
-        box_vectors = topology.box.get_vectors()
-        topology = self.run_atomtyping(topology, use_residue_map=use_residue_map)
-        system = self.createSystem(topology, *args, **kwargs)
+        box_vectors = top.box.get_vectors()
+        top = self.run_atomtyping(top, use_residue_map=use_residue_map)
+        top = self.parametrize_topology(top)
+        #system = self.createSystem(topology, *args, **kwargs)
 
-        structure = pmd.openmm.load_topology(topology=topology, system=system)
+        #structure = pmd.openmm.load_topology(topology=topology, system=system)
 
         '''
         Check that all topology objects (angles, dihedrals, and impropers)
         have parameters assigned. OpenMM will generate an error if bond parameters
         are not assigned.
         '''
-        data = self._SystemData
+        #data = self._SystemData
 
-        if data.bonds:
-            missing = [b for b in structure.bonds
-                       if b.type is None]
-            if missing:
-                nmissing = len(structure.bonds) - len(missing)
-                msg = ("Parameters have not been assigned to all bonds. "
-                       "Total system bonds: {}, Parametrized bonds: {}"
-                       "".format(len(structure.bonds), nmissing))
-                _error_or_warn(assert_bond_params, msg)
+        #if data.bonds:
+        #    missing = [b for b in structure.bonds
+        #               if b.type is None]
+        #    if missing:
+        #        nmissing = len(structure.bonds) - len(missing)
+        #        msg = ("Parameters have not been assigned to all bonds. "
+        #               "Total system bonds: {}, Parametrized bonds: {}"
+        #               "".format(len(structure.bonds), nmissing))
+        #        _error_or_warn(assert_bond_params, msg)
 
-        if data.angles and (len(data.angles) != len(structure.angles)):
-            msg = ("Parameters have not been assigned to all angles. Total "
-                   "system angles: {}, Parameterized angles: {}"
-                   "".format(len(data.angles), len(structure.angles)))
-            _error_or_warn(assert_angle_params, msg)
+        #if data.angles and (len(data.angles) != len(structure.angles)):
+        #    msg = ("Parameters have not been assigned to all angles. Total "
+        #           "system angles: {}, Parameterized angles: {}"
+        #           "".format(len(data.angles), len(structure.angles)))
+        #    _error_or_warn(assert_angle_params, msg)
 
-        proper_dihedrals = [dihedral for dihedral in structure.dihedrals
-                            if not dihedral.improper]
-        if data.propers and len(data.propers) != \
-                len(proper_dihedrals) + len(structure.rb_torsions):
-            msg = ("Parameters have not been assigned to all proper dihedrals. "
-                   "Total system dihedrals: {}, Parameterized dihedrals: {}. "
-                   "Note that if your system contains torsions of Ryckaert-"
-                   "Bellemans functional form, all of these torsions are "
-                   "processed as propers.".format(len(data.propers),
-                   len(proper_dihedrals) + len(structure.rb_torsions)))
-            _error_or_warn(assert_dihedral_params, msg)
+        #proper_dihedrals = [dihedral for dihedral in structure.dihedrals
+        #                    if not dihedral.improper]
+        #if data.propers and len(data.propers) != \
+        #        len(proper_dihedrals) + len(structure.rb_torsions):
+        #    msg = ("Parameters have not been assigned to all proper dihedrals. "
+        #           "Total system dihedrals: {}, Parameterized dihedrals: {}. "
+        #           "Note that if your system contains torsions of Ryckaert-"
+        #           "Bellemans functional form, all of these torsions are "
+        #           "processed as propers.".format(len(data.propers),
+        #           len(proper_dihedrals) + len(structure.rb_torsions)))
+        #    _error_or_warn(assert_dihedral_params, msg)
 
-        improper_dihedrals = [dihedral for dihedral in structure.dihedrals
-                              if dihedral.improper]
-        if data.impropers and len(data.impropers) != \
-                len(improper_dihedrals) + len(structure.impropers):
-            msg = ("Parameters have not been assigned to all impropers. Total "
-                   "system impropers: {}, Parameterized impropers: {}. "
-                   "Note that if your system contains torsions of Ryckaert-"
-                   "Bellemans functional form, all of these torsions are "
-                   "processed as propers".format(len(data.impropers),
-                   len(improper_dihedrals) + len(structure.impropers)))
-            _error_or_warn(assert_improper_params, msg)
+        #improper_dihedrals = [dihedral for dihedral in structure.dihedrals
+        #                      if dihedral.improper]
+        #if data.impropers and len(data.impropers) != \
+        #        len(improper_dihedrals) + len(structure.impropers):
+        #    msg = ("Parameters have not been assigned to all impropers. Total "
+        #           "system impropers: {}, Parameterized impropers: {}. "
+        #           "Note that if your system contains torsions of Ryckaert-"
+        #           "Bellemans functional form, all of these torsions are "
+        #           "processed as propers".format(len(data.impropers),
+        #           len(improper_dihedrals) + len(structure.impropers)))
+        #    _error_or_warn(assert_improper_params, msg)
 
-        structure.bonds.sort(key=lambda x: x.atom1.idx)
-        structure.positions = positions
-        if box_vectors is not None:
-            structure.box_vectors = box_vectors
+        #structure.bonds.sort(key=lambda x: x.atom1.idx)
+        #structure.positions = positions
+        #if box_vectors is not None:
+        #    structure.box_vectors = box_vectors
         if references_file:
-            atom_types = set(atom.type for atom in structure.atoms)
+            #atom_types = set(atom.type for atom in structure.atoms)
+            atom_types = set(site.atom_type for site in top.sites)
             self._write_references_to_file(atom_types, references_file)
 
-        return structure
+        return top
 
-    def run_atomtyping(self, topology, use_residue_map=True):
+    def parametrize_topology(self, top):
+        """ Specify Potential terms for all sites, bonds, angles, etc in top"""
+        for site in top.sites:
+            site.atom_type = _parametrize_atomtype(site)
+
+        for bond in top.bonds:
+            bond.connection_type = _parametrize_bondtype(bond)
+
+        for angle in top.angles:
+            angle.connection_type = _parametrize_angletype(angle)
+
+        #Todo: dihedrals/impropers/etc 
+
+        return top
+
+    def _parametrize_angletype(self, angle):
+        """ Specify AngleType term for Angle"""
+        found_angletype = [angtype for angtype in self.angletypes 
+                if _matching_constituents(angle, angtype)]
+        if len(found_angletype) > 0:
+            raise FoyerError("Multiple AngleType parameters "
+                            "found for {}".format(angle))
+        else:
+            return found_angletype[0]
+
+    def _parametrize_bondtype(self, bond):
+        """ Specify BondType term for Bond"""
+        found_bondtype = [btype for btype in self.bondtypes 
+                if _matching_constituents(bond, btype)]
+        if len(found_bondtype) > 0:
+            raise FoyerError("Multiple BondType parameters "
+                            "found for {}".format(bond))
+        else:
+            return found_bondtype[0]
+
+    def _parametrize_atomtype(self, site):
+        """ Specify AtomType term for Site """
+        found_atomtype = [atype for atype in self.atomtypes
+                if atype.name == site.id]
+        if len(found_atomtype) > 0:
+            raise FoyerError("Multiple AtomType parameters "
+                            "found for {}".format(site.id))
+        else:
+            return found_atomtype[0]
+
+    def run_atomtyping(self, top, use_residue_map=True):
         """Atomtype the topology
 
         Parameters
         ----------
-        topology : topology.Topology
+        top : topology.Topology
             Molecular structure to find atom types of
         use_residue_map : boolean, optional, default=True
             Store atomtyped topologies of residues to a dictionary that maps
@@ -490,9 +635,9 @@ class Forcefield(object):
 
         #else:
         #    find_atomtypes(topology, forcefield=self)
-        find_atomtypes(topology, forcefield=self)
+        find_atomtypes(top, forcefield=self)
 
-        if not all([a.id for a in topology.sites][0]):
+        if not all([a.id for a in top.sites][0]):
             raise ValueError('Not all atoms in topology have atom types')
 
         return topology
@@ -760,3 +905,27 @@ class Forcefield(object):
                         ', '.join(sorted(atomtypes)) + '}')
                 bibtex_ref = bibtex_ref[:-2] + note + bibtex_ref[-2:]
                 f.write('{}\n'.format(bibtex_ref))
+
+def _parse_unyt(unyt_string):
+    split_index = unyt_string.find('*')
+    val = float(unyt_string[:split_index])
+    unit = u.Unit(unyt_string[split_index+1:])
+    return val * unit
+
+def _matching_constituents(connection, connectiontype):
+    """ Assert that the Connection's members' types are the same as those
+    presecribed in the ConnectionType """
+    # This is a list of strings based on connection member atomtype names
+    c_member_types = [a.atom_type.name for a in connection.connection_members]
+
+    # This is a list of strings based on the connectiontype types
+    ctype_member_types = [a for a in connectiontype.types]
+
+    # Check forward and reverse
+    # I think we can just do __eq__ on the list because
+    # each item's __eq__ is compared, and each item should just be a string
+    if c_member_types == ctype_member_types:
+        return True
+    if c_member_types.reverse() == ctype_member_types:
+        return True
+    return False
