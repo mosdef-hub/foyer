a902aa82a94de0c827dadf7df81cde691ace40a3
Matthew W. Thompson <matt.thompson@vanderbilt.edu>
2019-04-10 11:01:54 -0500

    Just use builtin sets

--- foyer/smarts_graph.py
+++ foyer/smarts_graph.py
@@ -4,7 +4,6 @@ import sys
 
 import networkx as nx
 from networkx.algorithms import isomorphism
-from boltons.setutils import IndexedSet
 import parmed.periodic_table as pt
 
 from foyer.smarts import SMARTS
@@ -313,8 +312,8 @@ def _prepare_atoms(topology, compute_cycles=False):
             if compute_cycles:
                 atom.cycles = set()
             if not has_whitelists:
-                atom.whitelist = IndexedSet()
-                atom.blacklist = IndexedSet()
+                atom.whitelist = set()
+                atom.blacklist = set()
 
     if compute_cycles:
         bond_graph = nx.Graph()
