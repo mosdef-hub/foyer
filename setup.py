f91fc5dd116af4c537096282c34129f02a7f029f
Matthew W. Thompson <matt.thompson@vanderbilt.edu>
2019-05-02 22:22:14 -0500

    Bump to version 0.6.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ import subprocess
 from setuptools import setup, find_packages
 
 #####################################
-VERSION = "0.6.0"
+VERSION = "0.6.1"
 ISRELEASED = True
 if ISRELEASED:
     __version__ = VERSION
