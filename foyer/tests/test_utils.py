7bc7d739cd37bd9e98853752ec8efc7cb431aeab
Matthew W. Thompson <matt.thompson@vanderbilt.edu>
2019-09-26 09:25:47 -0500

    Test symmetry of nbfixes

--- foyer/tests/test_utils.py
+++ foyer/tests/test_utils.py
@@ -27,3 +27,8 @@ def test_apply_nbfix():
                 atom.atom_type.nbfix['opls_140'][:2],
                 [0.44898481932374923, 50.0]
             )
+        elif atom.atom_type.name == 'opls_140':
+            assert np.allclose(
+                atom.atom_type.nbfix['opls_135'][:2],
+                [0.44898481932374923, 50.0]
+            )
