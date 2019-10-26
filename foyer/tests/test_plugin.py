b6941b35f5bb20dbc2c7e05bbf6100bf0879be3f
Matthew W. Thompson <matt.thompson@vanderbilt.edu>
2019-10-26 13:37:13 -0500

    Update test to check more internals

--- foyer/tests/test_plugin.py
+++ foyer/tests/test_plugin.py
@@ -1,7 +1,18 @@
 import pytest
+import foyer
 
 
 def test_basic_import():
-    import foyer
     assert 'forcefields' in dir(foyer)
-    import foyer.forcefields.forcefields
+
+
+@pytest.mark.parametrize('ff_name', ['OPLSAA', 'TRAPPE_UA'])
+def test_forcefields_exist(ff_name):
+    ff_name in dir(foyer.forcefields)
+
+
+def test_load_forcefield():
+    OPLSAA = foyer.forcefields.get_forcefield(name='oplsaa')
+    TRAPPE_UA = foyer.forcefields.get_forcefield(name='trappe-ua')
+    with pytest.raises(ValueError):
+        foyer.forcefields.get_forcefield('bogus_name')
