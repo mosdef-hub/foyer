ad44c4d4c92b19c0c2d012e0b2952f146bbb7617
Matthew W. Thompson <matt.thompson@vanderbilt.edu>
2019-10-30 20:40:58 -0500

    Collect and evaluate loader functions

--- foyer/tests/test_plugin.py
+++ foyer/tests/test_plugin.py
@@ -6,9 +6,11 @@ def test_basic_import():
     assert 'forcefields' in dir(foyer)
 
 
-@pytest.mark.parametrize('ff_loader', ['load_OPLSAA', 'load_TRAPPE_UA'])
-def test_forcefields_exist(ff_loader):
-    assert ff_loader in dir(foyer.forcefields)
+def test_loading_forcefields():
+    #funcs = [func for func in dir(foyer.forcefields) if 'load' in func and '__' not in func]
+    for func in dir(foyer.forcefields):
+        if 'load_' in func and '__' not in func:
+            eval('foyer.forcefields.' + func)()
 
 
 def test_load_forcefield():
