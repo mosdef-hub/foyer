2678a39614c338cc2cbeab2619a905e90c7195e1
Parashara Shamaprasad <p.shama@vanderbilt.edu>
2019-11-22 12:04:22 -0600

    Make plugin_dir reference the directory instead of a file.

--- foyer/plugins.py
+++ foyer/plugins.py
@@ -1,5 +1,6 @@
 import foyer
 import glob
+import os
 
 def collect_plugins(plugin_names=None):
     """
@@ -25,6 +26,7 @@ def collect_plugins(plugin_names=None):
         plugin_name = "_".join(plugin_func.split("_")[1:])
         # TODO: plugin_version = get_version_info
         plugin_dir = eval("foyer.forcefields.{}.__globals__['__file__']".format(plugin_func))
+        plugin_dir = os.path.dirname(plugin_dir)
         # TODO: plugin_xml_path = get_xml_path
         # This assumes that plugin directory tree is consistent.
         # Does not consider versioned FFs.
