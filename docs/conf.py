1f1dcfc649198853b36fedbf05820e2b6dac1d09
Umesh <umesh.timalsina@vanderbilt.edu>
2020-02-18 11:39:18 -0600

    Write Version information before importing foyer, remove numpydoc in favor of napoleon, remove docs build from travis

--- docs/conf.py
+++ docs/conf.py
@@ -16,9 +16,15 @@
 
 import os
 import sys
+import pathlib
 
 sys.path.insert(0, os.path.abspath('..'))
 sys.path.insert(0, os.path.abspath('sphinxext'))
+
+base_path = pathlib.Path(__file__).parent
+os.system('python {} --name'.format((base_path / '../setup.py').resolve()))
+
+
 import foyer
 
 
