b1f71e8401c169d509c8be1ea41313b779df224e
bc118 <crawfordb118@gmail.com>
2021-06-29 17:29:17 -0400

    fixed a few errors for pre-commit

--- foyer/utils/check_xml_dihedrals_RB_to_OPLS.py
+++ foyer/utils/check_xml_dihedrals_RB_to_OPLS.py
@@ -3,14 +3,119 @@ import os
 import xml.etree.ElementTree as ET
 from foyer.forcefields import forcefields
 from foyer.forcefields.forcefields import get_forcefield
-from mbuild.utils.conversion import RB_to_OPLS
+#from mbuild.utils.conversion import RB_to_OPLS
 from warnings import warn
 
+# ****************************************************************************
+# remove this RB_to_OPLS once the new version of foyer imports the new mBuild (START)
+# ****************************************************************************
+def RB_to_OPLS(
+    c0,
+    c1,
+    c2,
+    c3,
+    c4,
+    c5,
+    error_tolerance=1e-4,
+    error_if_outside_tolerance=True,
+):
+    r"""Convert Ryckaert-Bellemans type dihedrals to OPLS type.
+
+    .. math::
+    RB_{torsions} &= c_0 + c_1*cos(psi) + c_2*cos(psi)^2 + c_3*cos(psi)^3 + \\
+                  &= c_4*cos(psi)^4 + c_5*cos(psi)^5
+
+    .. math::
+    OPLS_torsions &= \frac{f_0}{2} + \frac{f_1}{2}*(1+cos(t)) + \frac{f_2}{2}*(1-cos(2*t)) + \\
+                  &= \frac{f_3}{2}*(1+cos(3*t)) + \frac{f_4}{2}*(1-cos(4*t))
+
+    where :math:`psi = t - pi = t - 180 degrees`
+
+    Parameters
+    ----------
+    c0, c1, c2, c3, c4, c5 : Ryckaert-Belleman coefficients (in kcal/mol)
+    error_tolerance : float, default=1e-4
+        The acceptable absolute tolerance between the RB to OPLS conversion.
+        Any value entered is converted to an absolute value.
+    error_if_outside_tolerance : bool, default=True
+        This variable determines whether to provide a ValueError if the RB to OPLS
+        conversion is greater than the error_tolerance term (i.e., error_if_outside_tolerance=True),
+        or a warning if the RB to OPLS conversion is greater than the error_tolerance term
+        (i.e., error_if_outside_tolerance=False).
+
+    Returns
+    -------
+    opls_coeffs : np.array, shape=(5,)
+        Array containing the OPLS dihedrals coeffs f0, f1, f2, f3, and f4
+        (in kcal/mol).
+
+
+    Notes
+    -----
+    c5 must equal zero, or this conversion is not possible.
+
+    (c0 + c1 + c2 + c3 + c4 + c5) must equal zero, to have the exact
+    energies represented in the dihedral.
+
+    NOTE: fO IS TYPICALLY NOT IN THE OPLS DIHEDRAL EQUATION AND IS
+    ONLY USED TO TEST IF THIS FUNCTION CAN BE UTILIZED, AND
+    DETERMINE THE fO VALUE FOR LATER ENERGY SCALING IN MOLECULAR DYNAMICS
+    (MD) SIMULATIONS. THIS FUNCTION TESTS IF f0 IS ZERO (f0=0).
+
+    .. warning:: The :math:`\frac{f_{0}}{2}` term is the constant for the OPLS dihedral equation.
+        If the f0 term is not zero, the dihedral is not an exact conversion;
+        since this constant does not contribute to the force equation,
+        this should provide matching results for MD, but the energy for each
+        dihedral will be shifted by the :math:`\frac{f_{0}}{2}` value.
+    """
+    if not isinstance(error_tolerance, float):
+        raise TypeError(
+            f"The error_tolerance variable must be a float, is type {type(error_tolerance)}."
+        )
+    error_tolerance = abs(error_tolerance)
+
+    if not isinstance(error_if_outside_tolerance, bool):
+        raise TypeError(
+            f"The text_for_error_tolerance variable must be a bool, is type {type(error_if_outside_tolerance)}."
+        )
+
+    if not np.all(np.isclose(c5, 0, atol=1e-10, rtol=0)):
+        raise ValueError(
+            "c5 must equal zero, so this conversion is not possible."
+        )
+
+    f0 = 2.0 * (c0 + c1 + c2 + c3 + c4 + c5)
+    if not np.all(np.isclose(f0 / 2, 0, atol=error_tolerance, rtol=0)):
+        text_for_error_tolerance = (
+            "f0 = 2 * ( c0 + c1 + c2 + c3 + c4 + c5 ) is not zero. "
+            "The f0/2 term is the constant for the OPLS dihedral. "
+            "Since the f0 term is not zero, the dihedral is not an "
+            "exact conversion; since this constant does not contribute "
+            "to the force equation, this should provide matching results "
+            "for MD, but the energy for each dihedral will be shifted "
+            "by the f0/2 value."
+        )
+        if error_if_outside_tolerance is True:
+            raise ValueError(text_for_error_tolerance)
+        elif error_if_outside_tolerance is False:
+            warn(text_for_error_tolerance)
+
+    f1 = -2 * c1 - (3 * c3) / 2
+    f2 = -c2 - c4
+    f3 = -c3 / 2
+    f4 = -c4 / 4
+    return np.array([f0, f1, f2, f3, f4])
+# ****************************************************************************
+# remove this RB_to_OPLS once the new version of foyer imports the new mBuild (END)
+# ****************************************************************************
 
 def _test_xml_dihedrals(xml_file_directory_and_filename, output_file_name,
-                     error_tolerance_rb_to_opls=1e-4,
-                     ):
-    r"""In in file function for the conversion of the Ryckaert-Bellemans (RB) type to OPLS
+                        error_tolerance_rb_to_opls=1e-4,
+                        ):
+    r"""
+    Tests a single FF, comparing the RB to standard OPLS conversion for accuracy and generates dihedral error files
+
+    Checks the conversion of the Ryckaert-Bellemans (RB) type to OPLS
     type dihedrals (i.e., the mBuild RB_to_OPLS function) for errors and generates ouput
     files with the errors for the single xml file provided, containing conversion errors
     and their info.
@@ -272,7 +377,10 @@ def _test_xml_dihedrals(xml_file_directory_and_filename, output_file_name,
 
 
 def run_xml_test(xml_and_error_file_dict, error_tolerance_rb_to_opls=1e-4):
-    r"""In in file function for the conversion of the Ryckaert-Bellemans (RB) type to OPLS
+    r"""
+    Tests multiple FFs, comparing the RB to standard OPLS conversion for accuracy and generates dihedral error files
+
+    Checks the conversion of the Ryckaert-Bellemans (RB) type to OPLS
     type dihedrals (i.e., the mBuild RB_to_OPLS function) for errors and generates ouput
     files with the errors for each xml file provided, containing conversion errors
     and their info.
@@ -363,12 +471,14 @@ def run_xml_test(xml_and_error_file_dict, error_tolerance_rb_to_opls=1e-4):
                             error_tolerance_rb_to_opls
                             )
 
-
 def test_xml_dihedral_rb_to_opls(xml_filename,
                                  error_filename,
                                  non_exact_conversion_list,
-                                 error_tolerance_rb_to_opls=1e-4):
-    """
+                                 error_tolerance_rb_to_opls=1e-4
+                                 ):
+    r"""
+    Tests and compare RB to standard OPLS conversion for accuracy and against with known errors
+
     Test that all the xml dihedrals either pass the RB to standard OPLS conversion
     via the mBuild RB_to_OPLS function, or they are in the know list that is not
     a direct analytical conversion.
