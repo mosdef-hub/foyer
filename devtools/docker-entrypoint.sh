dfbcbd04e246a7c2b01500a32db6441457bed752
Justin Gilmer <justin.b.gilmer@vanderbilt.edu>
2021-08-13 12:58:57 -0500

    Update entrypoint and variable expansion

--- devtools/docker-entrypoint.sh
+++ devtools/docker-entrypoint.sh
@@ -2,7 +2,7 @@
 
 . /opt/conda/etc/profile.d/conda.sh
 conda activate base
-conda activate foyer-docker
+conda activate foyer-dev
 
 if [ "$@" == "jupyter" ]; then
 	jupyter notebook --no-browser --notebook-dir /home/anaconda/data --ip="0.0.0.0"
