5fed7d3d4f6f6b46412f9e6d9e2dd0d442e8e398
Umesh Timalsina <umesh.timalsina@vanderbilt.edu>
2021-11-17 14:39:19 -0600

    Bump to version 0.10.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from __future__ import print_function
 from setuptools import find_packages, setup
 
 #####################################
-VERSION = "0.9.4"
+VERSION = "0.10.0"
 ISRELEASED = True
 if ISRELEASED:
     __version__ = VERSION
