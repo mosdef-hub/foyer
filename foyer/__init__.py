24c61b92e86e8dc1dfa6827606c5db4111c48874
Co Quach <daico007@gmail.com>
2022-09-13 13:01:23 -0500

    Bump to version 0.11.3

--- foyer/__init__.py
+++ foyer/__init__.py
@@ -2,6 +2,6 @@
 from foyer.forcefield import Forcefield
 from foyer.forcefields import forcefields
 
-__version__ = "0.11.2"
+__version__ = "0.11.3"
 
 __all__ = ("Forcefield", "forcefields", "__version__")
