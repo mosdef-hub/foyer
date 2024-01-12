f77990d1a564e77f1f3f030d778ad93d2ab1c808
Co Quach <daico007@gmail.com>
2024-01-11 23:09:00 -0600

    Bump to version 0.12.1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,7 +33,7 @@ project = "foyer"
 copyright = "2014-2020, Vanderbilt University"
 
 # The short X.Y version
-version = "0.12.0"
+version = "0.12.1"
 # The full version, including alpha/beta/rc tags
 release = version
 
